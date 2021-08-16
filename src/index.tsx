c25f4286b12f2b3695151f9b1f9c139123ac9689
Marketgai <marcusalee@gmail.com>
2021-08-16 13:25:45 -0700

    throwing router errors

---
+++ src/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import App from './App';
+import { Home, SignIn, Dashboard } from './components'
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <Switch>
+          <Route exact path='/'>
+            <Home title={'Home'} />
+          </Route>
+          <Route path='/dashboard'>
+            <Dashboard />
+          </Route>
+          <Route path='/signin'>
+            <SignIn />
+          </Route>
+      </Switch>
+    </Router>
+    <Home title='scholar string' />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
